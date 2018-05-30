33d58c4fa6547e99d72d29ee3cea6b0ec643244a
Mathew0876 <38885367+Mathew0876@users.noreply.github.com>
2018-05-30 14:24:11 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WhiteBoard1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.colourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eraserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.brushWidthToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.colourToolStripMenuItem,
+            this.eraserToolStripMenuItem,
+            this.brushWidthToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1151, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.quitToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(90, 38);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(324, 38);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // colourToolStripMenuItem
+            // 
+            this.colourToolStripMenuItem.Name = "colourToolStripMenuItem";
+            this.colourToolStripMenuItem.Size = new System.Drawing.Size(98, 38);
+            this.colourToolStripMenuItem.Text = "Colour";
+            this.colourToolStripMenuItem.Click += new System.EventHandler(this.colourToolStripMenuItem_Click);
+            // 
+            // eraserToolStripMenuItem
+            // 
+            this.eraserToolStripMenuItem.Name = "eraserToolStripMenuItem";
+            this.eraserToolStripMenuItem.Size = new System.Drawing.Size(90, 38);
+            this.eraserToolStripMenuItem.Text = "Eraser";
+            this.eraserToolStripMenuItem.Click += new System.EventHandler(this.eraserToolStripMenuItem_Click);
+            // 
+            // brushWidthToolStripMenuItem
+            // 
+            this.brushWidthToolStripMenuItem.Name = "brushWidthToolStripMenuItem";
+            this.brushWidthToolStripMenuItem.Size = new System.Drawing.Size(163, 38);
+            this.brushWidthToolStripMenuItem.Text = "Brush Width:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(31, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1089, 803);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(490, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Tag = "Brush Width";
+            this.textBox1.Text = "3";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "WhiteBoard";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1151, 910);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "WhiteBoard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem colourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eraserToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem brushWidthToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WhiteBoard1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // creates the window and the drawing surface
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }
+
+        // drawing surface
+        // pen used for drawing
+        Graphics g;
+        Pen p = new Pen(Color.Black, 2);
+        private bool mouse_down = false;
+
+        Bitmap bmp = new Bitmap(1089, 803);
+        //C:\Users\Mathew\OneDrive\Pictures
+
+
+        // starting coordinates for the line
+        int x_cor;
+        int y_cor;
+
+        // when the mouse is down
+        // save inital coordinates
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_down = true;
+            x_cor = e.X;
+            y_cor = e.Y;
+
+        }
+
+        // when mouse is released, exit drawing mode
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_down = false;
+        }
+
+        // when the mouse moves
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // if in drawing mode
+            // draw a line from the previous point to the current point
+            // update the inital point to the current point
+            if (mouse_down)
+            {
+                g.DrawLine(p, new Point(x_cor, y_cor), new Point(e.X, e.Y));
+
+                x_cor = e.X;
+                y_cor = e.Y;
+            }
+
+        }
+
+        // clears the panel
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+        }
+
+        // closes the application
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // changes the colour of the pen
+        private void colourToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            if (c.ShowDialog() == DialogResult.OK)
+            {
+                p = new Pen(c.Color, p.Width);
+            }
+        }
+
+        // creates an eraser by setting the pen colour to white
+        private void eraserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.White, p.Width);
+        }
+
+        // changes the width of the pen or eraser
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // catches error of invalied input null or less than 1
+            try
+            {
+                p = new Pen(p.Color, float.Parse(textBox1.Text));
+            }
+            catch { }
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            /**using (Bitmap graphicSurface = new Bitmap(panel1.Width, panel1.Height))
+            {
+                using (StreamWriter bitmapWriter = new StreamWriter("TEST.JPEG"))
+                {
+                    panel1.DrawToBitmap(graphicSurface, new Rectangle(0, 0, panel1.Width, panel1.Height));
+                    graphicSurface.Save(bitmapWriter.BaseStream, ImageFormat.Jpeg);
+                }
+            }*/
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "Select output file:";
+            dialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //Bitmap bmp = new Bitmap(panel1.Width, panel1.Height);
+                //panel1.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+
+                bmp.Save(dialog.FileName);// System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Configure the message box to be displayed
+            string messageBoxText = "Created May 2018" +
+                "\nVersion 1.0\nReleased May 30 2018\nWritten by Mathew Smith";
+            string caption = "White Board";
+            MessageBoxButtons button = MessageBoxButtons.OKCancel;
+
+            // Display message box
+            MessageBox.Show(messageBoxText, caption, button);
+        }
+    }
+}
